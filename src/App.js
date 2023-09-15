6ed573fcd1c69e326a9dfafd6a7c33ec734df400
Arash <arash8877@gmail.com>
2023-09-15 21:21:39 +0200

    completed subscription

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Navbar, Header, Features, Download, Faq, Footer } from './components/index'
+import { Navbar, Header, Features, Download, Subscribe, Faq, Footer } from './components/index'
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       </header>
       <Features/>
       <Download/>
+      <Subscribe/>
       <Faq/>
       <Footer/>
     </main>
